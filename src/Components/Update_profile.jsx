c6a12fcfa210d21c2c0851f209ade16fb2be9a10
Ravi Kumar <ravi14577@gmail.com>
2023-12-26 16:46:16 +0530

    section Update_profile

--- src/Components/Update_profile.jsx
+++ src/Components/Update_profile.jsx
@@ -3,23 +3,14 @@ import React from 'react'
 const Update_profile = () => {
   return (
     <>
-    <div className='w-full items-start h-screen flex'>
-    {/* left side containt */}
-    <div className='h-full flex flex-col p-20 text-center'>
-
-        <h1 className='text-blue-950 font-bold text-5xl p-5'> Welcome</h1>
-        <h2 className='text-green-950 text-3xl p-5'>Sign In</h2>
-        <h3 className='text-green-950 text-3xl p-5'>Sign In to Logo. to continue</h3>
-        <div className='flex flex-col text-center justify-center'>
-            <input type="email" placeholder='Email id' className='border p-3 m-3 rounded-lg border-black'/>
-            <input type="password" placeholder='Password'  className='border p-3 m-3 rounded-lg border-black' />
-           
+    <div>
+        <div>
+            <ul className='text-center justify-center pt-10'>
+                <li className='text-bold p-3 text-left'> Name</li>
+                <li><input type="text" className="border-black rounded-xl p-3 border" placeholder='Name' /></li>
+            </ul>
         </div>
-        
-        <button className='bg-blue-500 rounded-lg m-5 text-3xl p-5'> Sign Up</button>
-        
-</div>
-</div>
+    </div>
     
     </>
   )
