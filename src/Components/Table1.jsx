9be9e55fbbbdf0b3b2fe4f52a5edb7b25e1f222c
Ravi Kumar <ravi14577@gmail.com>
2023-12-29 14:22:20 +0530

    table useeffect adds and perform well

--- src/Components/Table1.jsx
+++ src/Components/Table1.jsx
@@ -3,20 +3,65 @@ import axios from "axios";
 
 const Table1 = () => {
   const [data, setData] = useState([]);
+  const [home, setHome] = useState("");
+  const [About, setAbout] = useState("");
+  const [contact, setContact] = useState("");
+  const [company, setCompany] = useState("");
+
   useEffect(() => {
-    axios.get("http://localhost:3000/users")
+    axios
+      .get("http://localhost:3000/users")
       .then((res) => setData(res.data))
-      .catch(er => console.log(er))
-  },[]);
+      .catch((er) => console.log(er));
+  }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const id = data[data.length - 1].id + 1;
+    axios
+      .post("http://localhost:3000/users", {
+        id: id,
+        home: home,
+        About: About,
+        contact: contact,
+        company: company,
+      })
+      .then((res) => console.log(res))
+      .catch((er) => console.log(er));
+  };
   return (
     <>
       <div>
         <div>
-          <input type="text" placeholder="company name" className="p-5 m-5 border-black border" />
-          <input type="text" placeholder="Home"className="p-5 m-5 border-black border" />
-          <input type="text" placeholder="About" className="p-5 m-5 border-black border"/>
-          <input type="text" placeholder="contact" className="p-5 m-5 border-black border"/>
-          <button className="bg-blue-500 border border-black py-5 px-8 m-5 rounded-full ">Add</button>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="company "
+              className="p-5 m-5 border-black border"
+              onChange={(e) => setCompany(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="Home"
+              className="p-5 m-5 border-black border"
+              onChange={(e) => setHome(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="About"
+              className="p-5 m-5 border-black border"
+              onChange={(e) => setAbout(e.target.value)}
+            />
+            <input
+              type="text"
+              placeholder="contact"
+              className="p-5 m-5 border-black border"
+              onChange={(e) => setContact(e.target.value)}
+            />
+            <button className="bg-blue-500 border border-black py-5 px-8 m-5 rounded-full ">
+              Add
+            </button>
+          </form>
         </div>
         <table>
           <thead>
