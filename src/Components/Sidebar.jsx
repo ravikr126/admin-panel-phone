cee88e24eeaa87f76dd1c077237f3d26e3b1e559
Ravi Kumar <ravi14577@gmail.com>
2023-12-30 14:12:11 +0530

    new branch v2

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,109 +1,68 @@
 import React from "react";
+import { useState } from "react";
 
 const Sidebar = () => {
+  const [open, setOpen] = useState(true);
+  const Menus = [
+    { title: "Dashboard", src: "Chart_fill" },
+    { title: "Inbox", src: "Chat" },
+    { title: "Accounts", src: "User", gap: true },
+    { title: "Schedule ", src: "Calendar" },
+    { title: "Search", src: "Search" },
+    { title: "Analytics", src: "Chart" },
+    { title: "Files ", src: "Folder", gap: true },
+    { title: "Setting", src: "Setting" },
+  ];
   return (
     <>
-      {/* <!-- component --> */}
-      <link
-        rel="stylesheet"
-        href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css"
-      />
-
-      <div class="min-h-screen flex flex-row ">
-        <div class="flex flex-col w-60 overflow-hidden bg-[#EEF5FF]">
-          <ul class="flex flex-col py-4 m-2">
-            <li className="rounded-full hover:bg-cyan-500 ">
-              <a
-                href="#"
-                class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800"
+      <div className="flex">
+        <div
+          className={` ${
+            open ? "w-72" : "w-20 "
+          } bg-dark-purple h-screen p-5  pt-8 relative duration-300`}
+        >
+          <img
+            src="./src/assets/control.png"
+            className={`absolute cursor-pointer -right-3 top-9 w-7 border-dark-purple
+           border-2 rounded-full  ${!open && "rotate-180"}`}
+            onClick={() => setOpen(!open)}
+          />
+          <div className="flex gap-x-4 items-center">
+            <img
+              src="./src/assets/logo.png"
+              className={`cursor-pointer duration-500 ${
+                open && "rotate-[360deg]"
+              }`}
+            />
+            <h1
+              className={`text-white origin-left font-medium text-xl duration-200 ${
+                !open && "scale-0"
+              }`}
+            >
+              Designer
+            </h1>
+          </div>
+          <ul className="pt-6">
+            {Menus.map((Menu, index) => (
+              <li
+                key={index}
+                className={`flex  rounded-md p-2 cursor-pointer hover:bg-light-white text-gray-300 text-sm items-center gap-x-4 
+              ${Menu.gap ? "mt-9" : "mt-2"} ${
+                  index === 0 && "bg-light-white"
+                } `}
               >
-                <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400">
-                  <i class="bx bx-home"></i>
+                <img src={`./src/assets/${Menu.src}.png`} />
+                <span
+                  className={`${!open && "hidden"} origin-left duration-200`}
+                >
+                  {Menu.title}
                 </span>
-                <span class="text-sm font-medium">Dashboard</span>
-              </a>
-            </li>
-            <li className="rounded-full hover:bg-cyan-500 ">
-              <a
-                href="#"
-                class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800"
-              >
-                <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400">
-                  <i class="bx bx-music"></i>
-                </span>
-                <span class="text-sm font-medium">Music</span>
-              </a>
-            </li>
-            <li className="rounded-full hover:bg-cyan-500 ">
-              <a
-                href="#"
-                class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800"
-              >
-                <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400">
-                  <i class="bx bx-drink"></i>
-                </span>
-                <span class="text-sm font-medium">Drink</span>
-              </a>
-            </li>
-            <li className="rounded-full hover:bg-cyan-500 ">
-              <a
-                href="#"
-                class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800"
-              >
-                <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400">
-                  <i class="bx bx-shopping-bag"></i>
-                </span>
-                <span class="text-sm font-medium">Shopping</span>
-              </a>
-            </li>
-            <li className="rounded-full hover:bg-cyan-500 ">
-              <a
-                href="#"
-                class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800"
-              >
-                <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400">
-                  <i class="bx bx-chat"></i>
-                </span>
-                <span class="text-sm font-medium">Chat</span>
-              </a>
-            </li>
-            <li className="rounded-full hover:bg-cyan-500 ">
-              <a
-                href="#"
-                class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800"
-              >
-                <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400">
-                  <i class="bx bx-user"></i>
-                </span>
-                <span class="text-sm font-medium">Profile</span>
-              </a>
-            </li>
-            <li className="rounded-full hover:bg-cyan-500 ">
-              <a
-                href="#"
-                class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800"
-              >
-                <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400">
-                  <i class="bx bx-bell"></i>
-                </span>
-                <span class="text-sm font-medium">Notifications</span>
-                <span class="ml-auto mr-6 text-sm bg-red-100 rounded-full px-3 py-px text-red-500">
-                  5
-                </span>
-              </a>
-            </li>
-            <li className="rounded-full hover:bg-cyan-500 ">
-              <a
-                href="#"
-                class="flex flex-row items-center h-12 transform hover:translate-x-2 transition-transform ease-in duration-200 text-gray-500 hover:text-gray-800"
-              >
-                <span class="inline-flex items-center justify-center h-12 w-12 text-lg text-gray-400">
-                  <i class="bx bx-log-out"></i>
-                </span>
-                <span class="text-sm font-medium">Logout</span>
-              </a>
-            </li>
+              </li>
+            ))}
           </ul>
+        </div>
+        <div className="h-screen flex-1 p-7">
+          <h1 className="text-2xl font-semibold ">Home Page</h1>
         </div>
       </div>
     </>
