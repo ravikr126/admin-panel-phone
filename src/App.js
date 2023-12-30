43d470799bfe09bbb1693024c5105677592d87f3
Ravi Kumar <ravi14577@gmail.com>
2023-12-30 15:51:12 +0530

    new header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react'
 import Homes from './Components/Homes'
 import Header from './Components/Header'
-import Sidebar from './Components/Sidebar'
 
 const App = () => {
   return (
     <>
+    
     <Header/>
-    <Sidebar/>
     <Homes/>
 
     
